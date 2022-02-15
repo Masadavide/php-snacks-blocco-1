d8eb1dd2ec7d3691766891408b5883f0b043e66e
Masadavide <davide.masadm@gmail.com>
2022-02-15 17:12:20 +0100

    finito

---
+++ snackD.php
@@ -0,0 +1,7 @@
+<?php
+    $paragrafo = "Tutti abbiamo udito la donnetta che dice: ''Oh, è terribile quel che fanno questi giovani a se stessi, secondo me la droga è una cosa tremenda''. Poi tu la guardi, la donna che parla in questo modo: è senza occhi, senza denti, senza cervello, senz'anima, senza culo, né bocca, né calore umano, né spirito, niente, solo un bastone, e ti chiedi come avran fatto a ridurla in quello stato i tè con i pasticcini e la chiesa.";
+    
+    $divisione = explode(".", $paragrafo);
+
+    var_dump ($divisione);
+?>
