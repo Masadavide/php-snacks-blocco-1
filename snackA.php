f250e766e20726d1ee289d751fbec7516cf560b4
Masadavide <davide.masadm@gmail.com>
2022-02-15 15:03:54 +0100

    php-snacks-blocco-1

---
+++ snackA.php
@@ -0,0 +1,29 @@
+<?php
+$array = [
+    [
+        "casa" => "Olimpia Milano",
+        "ospite" => "Cantù",
+        "punticasa" => "55",
+        "puntiospite" => "60",
+    ],
+    [
+        "casa" => "Virtus Bologna",
+        "ospite" => "Trento",
+        "punticasa" => "73",
+        "puntiospite" => "79",
+    ],
+    [
+        "casa" => "Varese",
+        "ospite" => "Dinamo Sassari",
+        "punticasa" => "89",
+        "puntiospite" => "78",
+    ],
+];
+
+for ($i = 0; $i < count($array);$i++){
+    echo $array[$i]["casa"].' - '.$array[$i]["ospite"].' | '.$array[$i]["punticasa"].'-'.$array[$i]["puntiospite"];
+    echo "</br>";
+}
+?>
+
+
